88f7d3b13fe7b240638dac0c9067122d494b3a0b
vinay kumar <vinay.kumar@technogetic.com>
2022-07-25 13:21:04 +0530

    changes

--- src/pages/common/header.js
+++ src/pages/common/header.js
@@ -1,32 +1,21 @@
 import React from "react";
-//import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { Container,  Nav,  Navbar, NavDropdown } from "react-bootstrap";
-//import { BiUserCircle } from "react-icons/bi";
-//import { Link } from "react-router-dom";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
 import { Searchbar } from "../../components/searchbar/Searchbar";
 import "./style.css";
 import { ImProfile } from "react-icons/im";
 import { AiOutlineSetting } from "react-icons/ai";
+import HamburgurBtn from "../../components/HamburgurBtn";
 
 export const Header = () => {
-  // cfhgj
+  // yhuji
   return (
-    <div >
-    <Navbar collapseOnSelect bg="light" expand="lg">
-      <Container fluid>
-      <h4>dashboard</h4>
-      <Searchbar /> 
-        {/* {/* <Navbar.Brand href="#">dashboard</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Searchbar />  */}
-         
-          {/* <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          ></Nav> */}
-          {/* <Navbar.Collapse > */}
+    <div>
+      <Navbar collapseOnSelect className="nav">
+        <Container fluid>
+          <HamburgurBtn />
+          <h4>dashboard</h4>
+          <Searchbar />
           <Nav>
             <img
               src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp"
@@ -46,11 +35,11 @@ export const Header = () => {
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action3">Log Out</NavDropdown.Item>
             </NavDropdown>
-            </Nav>
+          </Nav>
           {/* </Navbar.Collapse> */}
-        {/* </Navbar.Collapse> */}
-      </Container>
-    </Navbar>
+          {/* </Navbar.Collapse> */}
+        </Container>
+      </Navbar>
     </div>
   );
 };
