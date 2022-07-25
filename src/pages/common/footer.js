c73fe4c596f43fefce2201a00bcf2b060e2944ff
vinay kumar <vinay.kumar@technogetic.com>
2022-07-25 13:11:38 +0530

    checking

--- src/pages/common/footer.js
+++ src/pages/common/footer.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Nav } from "react-bootstrap";
+import React from 'react'
+import { Nav } from 'react-bootstrap'
 
 export const Footer = () => {
   return (
     <div>
-      <Nav className="navbar fixed-bottom bg-light">
-        <div className="container-fluid">
-          <h5 className="navbar-brand">Fixed bottom</h5>
-        </div>
-      </Nav>
+        <Nav className="navbar fixed-bottom bg-light">
+  <div className="container-fluid">
+    <h5 className="navbar-brand" >Fixed bottom</h5>
+  </div>
+</Nav>
     </div>
-  );
-};
+  )
+}
