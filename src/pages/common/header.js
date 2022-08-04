c4bdadd9a3711947e507d2ae5e9aee0bcee050a3
vinay kumar <vinay.kumar@technogetic.com>
2022-08-04 17:33:35 +0530

    Header restore done

--- src/pages/common/header.js
+++ src/pages/common/header.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+//import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container,  Nav,  Navbar, NavDropdown } from "react-bootstrap";
+//import { BiUserCircle } from "react-icons/bi";
+//import { Link } from "react-router-dom";
 import { Searchbar } from "../../components/searchbar/Searchbar";
 import "./style.css";
 import { ImProfile } from "react-icons/im";
@@ -7,11 +10,22 @@ import { AiOutlineSetting } from "react-icons/ai";
 
 export const Header = () => {
   return (
-    <div>
-      <Navbar collapseOnSelect bg="light" expand="lg">
-        <Container fluid>
-          <h4>dashboard</h4>
-          <Searchbar />
+    <div >
+    <Navbar collapseOnSelect bg="light" expand="lg">
+      <Container fluid>
+      <h4>dashboard</h4>
+      <Searchbar /> 
+        {/* {/* <Navbar.Brand href="#">dashboard</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Searchbar />  */}
+         
+          {/* <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          ></Nav> */}
+          {/* <Navbar.Collapse > */}
           <Nav>
             <img
               src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp"
@@ -31,11 +45,11 @@ export const Header = () => {
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action3">Log Out</NavDropdown.Item>
             </NavDropdown>
-          </Nav>
+            </Nav>
           {/* </Navbar.Collapse> */}
-          {/* </Navbar.Collapse> */}
-        </Container>
-      </Navbar>
+        {/* </Navbar.Collapse> */}
+      </Container>
+    </Navbar>
     </div>
   );
 };
